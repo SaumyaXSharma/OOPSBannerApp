86599e682aca842f52fc7efd2450d74ce759314c
Saumya Sharma <as5960@srmist.edu.in>
2026-02-18 05:17:18 +0530

    Coded UC2 to Render OOPS as Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,19 @@
-/**
- * OOPSBannerApp UC1 OOPS Banner Display Application
- *
- * This class demonstrates a simple Java application that displays the 
- * Object Oriented Programming System OOPS acronym to the console.
- *
- * @author Developer
- * @version 1.0
- */
+
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        // Line 1
+        System.out.println("  ***** " + "  ***** " + " ******* " + "  ****** ");
+        // Line 2
+        System.out.println(" **   ** " + " **   ** " + "**    ** " + " ** ");
+        // Line 3
+        System.out.println(" **   ** " + " **   ** " + "**    ** " + " ** ");
+        // Line 4
+        System.out.println(" **   ** " + " **   ** " + "******* " + "   ***** ");
+        // Line 5
+        System.out.println(" **   ** " + " **   ** " + "** " + "             **");
+        // Line 6  
+        System.out.println(" **   ** " + " **   ** " + "** " + "            **");
+        // Line 7
+        System.out.println("  ***** " + "   ***** " +  "** " +   "       ****** ");
     }
 }
